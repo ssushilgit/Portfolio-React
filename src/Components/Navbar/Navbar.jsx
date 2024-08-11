eb8fbded74e1b4e834c9474122c66d50ac5a159e
ssushilgit <shresthasushil741@gmail.com>
2024-08-11 23:29:04 +0545

    Initial setup and Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>Sushil Shrestha</h1>
+      <ul className='nav-menu'>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">
+        Connect With Me
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
